4e1890f0d3eba9adcfc171343b6d96fce6ca13c8
Clewerton Roque <48888681+clewertonx1@users.noreply.github.com>
2019-09-03 16:20:16 -0300

    Add files via upload

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,35 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+	async store(req, res) {
+		const { user } = req.headers
+		const { devId } = req.params
+
+		const loggedDev = await Dev.findById(user)
+		const targetDev = await Dev.findById(devId)
+
+		if (!targetDev) {
+			return res.status(400).json({ error: 'Dev nao existe' })
+		}
+
+		
+		if (targetDev.likes.includes(loggedDev._id)) {
+			
+			const loggedSocket = req.connectedUsers[user]
+			const targetSocket = req.connectedUsers[devId]
+
+			if (loggedSocket) {	
+				req.io.to(loggedSocket).emit('match', targetDev)
+			}
+
+			if (targetSocket) {
+				req.io.to(targetSocket).emit('match', loggedDev)
+			}	
+		}
+		
+		loggedDev.likes.push(targetDev._id)
+		await loggedDev.save()
+
+		return res.json(loggedDev)
+	}
+};
